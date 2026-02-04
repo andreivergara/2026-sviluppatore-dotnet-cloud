ac7935db19cd785a43f45cbbd7c961bceeb0e771
unknown <vergaraandrei@gmail.com>
2026-02-04 09:45:04 +0100

    Commit esercitazione nome cognome

--- esercitazioni/esercitazione-02/Program.cs
+++ esercitazioni/esercitazione-02/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.Write("Inserisci nome!");
+
+string nome = Console.ReadLine();
+
+Console.Write("Inserisci cognome");
+
+string cognome = Console.ReadLine();
+
+Console.WriteLine("$Benvenuto"{nome}{cognome});
+
